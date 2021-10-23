bcfe112cae67e106c19575b0557f7b09bac932af
omarkady <omarkadytt@gmail.com>
2021-10-23 12:43:40 +0200

    Design and implement api models, then add to admin tool.

--- api/models.py
+++ api/models.py
@@ -3,10 +3,13 @@ from users.models import CustomUser
 # Create your models here.
 
 class Feed(models.Model):
+    feed_url = models.URLField(max_length=400)
+
+class Content(models.Model):
     title = models.CharField(max_length=300, unique=True)
     url = models.URLField(max_length=400)
     owener = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    rss_url = models.URLField()
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title

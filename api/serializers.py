fba7e231e379905c2ee36cb5832a987ecc1e5860
omarkady <omarkadytt@gmail.com>
2021-10-23 13:39:58 +0200

    Delete old view, serializers and urls from api

--- api/serializers.py
+++ api/serializers.py
@@ -1,15 +1,3 @@
-from .models import Feed
-from django.db.models import fields
 from rest_framework import serializers
 
 # List feed serializer
-class FeedSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Feed
-        fields = ('title', 'url',)
-
-# Create feed form serializer
-class CreateFormSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=250)
-    url = serializers.CharField()
--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, include
-from .views import CreateFeedView
 
 urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('create-feed/', CreateFeedView.as_view(), name='create-feed'),
 ]
